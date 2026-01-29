387a8c8a4b37ae6c0205b337c1d8172c33203e9e
Nemo <menard.annec@gmail.com>
2026-01-29 12:27:33 +0100

    delete

--- src/modules/cuisine/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Package Cuisine - Modules séparés pour une meilleure organisation
-
-Structure:
-- recettes.py: Gestion des recettes
-- inventaire.py: Gestion du stock
-- planning.py: Planning hebdomadaire
-- courses.py: Liste de courses
-"""
